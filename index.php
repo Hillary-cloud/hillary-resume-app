441f8aa964e96ae4b34a0a6cbd3a514a5bc2517a
Hillary-cloud <85568261+Hillary-cloud@users.noreply.github.com>
2021-08-22 00:14:30 +0100

    updated index.php file

--- index.php
+++ index.php
@@ -32,6 +32,8 @@ if(isset($_POST['submit'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.css" />
+    <script src="bootstrap/js/bootstrapjquery.js"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
     <title>my resume</title>
 </head>
 <body>
@@ -43,12 +45,19 @@ if(isset($_POST['submit'])){
 </head>
 
 <body>
-    <div class="container-fluid">
+    <div class="container-fluid bg-dark">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <nav class="navbar-brand"><?php echo "OBIORA HILLARY CHIJIOKE";?></nav>
-                <ul class="navbar-nav">
+            <nav class="navbar-brand text-warning">OBIORA HILLARY CHIJIOKE</nav>
+            <button class="navbar-toggler bg-secondary"  type="button" data-toggle="collapse" data-target="#mynavbarasupportedcontent">
+            <span class=" navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="mynavbarasupportedcontent">
+                <ul class="navbar-nav pull-left ml-5">
                     <li class="nav-item">
                         <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#skills">Skills</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#contactus">Contact Us</a>
@@ -60,40 +69,43 @@ if(isset($_POST['submit'])){
                         <a class="nav-link" href="https://www.linkedin.com/in/obi-larry-33ab11216">Linkedin</a>
                     </li>
                 </ul>
+                </div>
         </nav>
-        <div class="row">
+        <div class="row  ">
             <div class=" mt-2 col-lg-9 col-md-9 col-sm-12 col-xs-12">
-                <div class="card">
-                        <div class="card-body">
-                            <h1>Obiora Hillary Chijioke</h1>
-                            <em>Odim gate,University of Nigeria,Nsukka  Nsukka  Enugu,410001  08147078588  obicj97@gmail.com</em><hr>
-                            <h3>CAREER OBJECTIVE</h3>
+                <div class="card ">
+                        <div class="card-body text-light bg-dark">
+                            <h1 class="text-secondary">Obiora Hillary Chijioke</h1>
+                            <em>Odim gate,University of Nigeria,Nsukka  Nsukka  Enugu,410001  +2348147078588  obicj97@gmail.com</em><hr>
+                                <img src="hillary.jpg" class="img-fluid rounded mx-auto d-block" alt="Responsive image" width="150px">
+                            <h3 class="text-secondary">CAREER OBJECTIVE</h3>
                             <p>Hard-working developer with two year of experience and a proven knowledge of cellular communications, programming and web development. Aiming to leverage my skills to successfully fill the Backend web engineer role at your company.</p><hr>
-                            <h3>EXPERIENCE</h3>
+                            <h3 class="text-secondary">EXPERIENCE</h3>
                             <p>Centre for Distance & e-leaning,University of Nigeria, Nsukka.</p>
-                            <em>Web developer, Oct 2019 - July 2021</em>
-                            <ul>Manage a group of developers as the team leader</ul>
-                            <ul>Share functionalities to each team member</ul>
-                            <ul>Add new features to projects</ul>
+                            <em>Web developer, Oct 2019 - July 2021.</em>
+                            <ul>Manage a group of developers as the team leader.</ul>
+                            <ul>Share functionalities to each team member.</ul>
+                            <ul>Add new features to projects.</ul>
                             <ul>supervise completed works.</ul><hr>
-                            <h3>EDUCATION</h3>
+                            <h3 class="text-secondary">EDUCATION</h3>
                             <p>University of Nigeria,Nsukka Enugu state.</p>
-                            <em>Bachelor of Science (B.S) Computer Science (Expected graduation July 2022)</em>
-                            <ul>Relevant Coursework: Mathematics</ul><hr>
-                            <h3>ADDITIONAL SKILLS</h3>
-                            <ul>Good communication skill</ul>
-                            <ul>Good reasoning skill</ul>
+                            <em>Bachelor of Science (B.S) Computer Science (Expected graduation July 2022).</em>
+                            <ul>Relevant Coursework: Mathematics.</ul><hr>
+                            <h3 class="text-secondary">ADDITIONAL SKILLS</h3>
+                            <ul>Good communication skill.</ul>
+                            <ul>Good reasoning skill.</ul>
                             <ul>Efficient and reliable.</ul>
-                            <ul>Management skill</ul>
-                            <ul>Leadership skill</ul>
+                            <ul>Management skill.</ul>
+                            <ul>Leadership skill.</ul>
                         </div>
                     </div>
             </div>
 
             <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-            <h2 class="text-center bg-dark text-light mb-0">Professional skills</h2>
+            <a name="skills"></a> 
+            <h2 class="text-center text-primary mb-0">Professional skills</h2><br>
                 <ul class="nav flex-column nav-pills">
-                    <li class=" text-center  ">
+                    <li class="text-light text-center  ">
                         <p>HTML </p>
                         <hr>
                         <p>CSS</p>
@@ -102,13 +114,13 @@ if(isset($_POST['submit'])){
                         <hr>
                         <p>PHP</p>
                         <hr>
-                        <p>Jquery</p>
+                        <p>Javascript</p>
                         <hr>
                         <p>MYSQL database</p>
                         <hr>
                         <p>Laravel</p>
-                    </li>
-                </ul><hr>
+                    </li><hr class="bg-light">
+                </ul>
                 <?php 
                     if(isset($_SESSION['status'])){
                         ?>
@@ -119,22 +131,22 @@ if(isset($_POST['submit'])){
                         <?php
                     }
                 ?>
-                <h4><strong><a name="contactus"></a> Leave us a message</strong></h4><hr>
-                 <form action="" method="post">
+                <h4 class="text-primary"><strong><a name="contactus"></a> Leave us a message</strong></h4><hr>
+                 <form class="text-light" action="" method="post">
                     <label for="">Full Name</label><br>
-                    <input type="text" class="form-control" placeholder="fullname" name="name" required><br>
+                    <input type="text" class="form-control" placeholder="Fullname" name="name" required><br>
                     <label for="">Email</label><br>
-                    <input type="text"  class="form-control" placeholder="email" name="email" required><br>
+                    <input type="text"  class="form-control" placeholder="Email" name="email" required><br>
                     <label for="">Message</label><br>
                     <textarea name="message"  class="form-control" id="" cols="30" rows="5" placeholder="Enter your message" required></textarea><br><br>
                     <button class="btn btn-success" name="submit">SEND</button>
                  </form><hr>
-                 <a href="https://zuri.team"><p>Tasked by:</p><img src="zuri.png" style="width:100px; height:100px"alt=""></a>
+                 <a  href="https://zuri.team"><p>Tasked by:</p><img src="zuri.png"  style="width:100px; height:100px"alt=""></a>
+                 
             </div>
         </div>
-        <br>
-
-        <div class="row bg-dark text-light">
+        <br><hr class="text-light">
+        <div class="row text-center bg-dark text-light">
             <p>&copy : Ceejay 2021</p>
         </div>
     </div>
